119057259ea29fa5b726065028896548368a2867
NastasiSantino <snastasi@itba.edu.ar>
2026-08-28 19:53:39 -0300

    Driver Lector de Tarjeta (Version 1)

---
+++ TP1_code/source/card_reader.c
@@ -0,0 +1,87 @@
+#include "card_reader.h"
+
+static volatile uint32_t dataBuffer[7] = {0};
+static volatile uint8_t bitCount;
+static volatile bool reading;
+static volatile bool dataReady;
+
+static pin_t data;
+static pin_t enable;
+
+void enable_IRQHandler(void);
+void clock_IRQHandler(void);
+
+void card_reader_INIT(pin_t enablePin, pin_t clockPin, pin_t dataPin)
+{
+	gpioMode(enablePin, INPUT);
+	gpioMode(clockPin, INPUT);
+	gpioMode(dataPin, INPUT);
+
+	data = dataPin;
+	enable = enablePin;
+
+	gpioIRQ(enablePin, GPIO_IRQ_MODE_BOTH_EDGES, enable_IRQHandler);
+	gpioIRQ(clockPin, GPIO_IRQ_MODE_FALLING_EDGE, clock_IRQHandler);
+
+
+	bitCount = 0;
+	reading = false;
+	dataReady = false;
+}
+
+bool data_ready(void)
+{
+	if(dataReady)
+	{
+		dataReady = false;
+		return true;
+	}
+
+	return false;
+}
+
+uint8_t get_data_length(void)
+{
+	return bitCount;
+}
+
+uint32_t * get_data(void)
+{
+	return dataBuffer;
+}
+
+void enable_IRQHandler(void)
+{
+	if(!gpioRead(enable))
+	{
+		reading = false;
+		dataReady = true;
+	} else
+	{
+		reading = true;
+		dataReady = false;
+		bitCount = 0;
+
+        for(uint8_t i = 0; i < 7; i++)
+        {
+            dataBuffer[i] = 0;
+        }
+	}
+
+}
+
+void clock_IRQHandler(void)
+{
+	if(reading)
+	{
+		if(bitCount < 224)
+		{
+			if(gpioRead(data))
+			{
+				dataBuffer[bitCount>>5] |= (1U << (bitCount & 0x1F));
+			}
+
+			bitCount++;
+		}
+	}
+}
---
+++ TP1_code/source/card_reader.h
@@ -0,0 +1,27 @@
+#ifndef CARD_READER_H_
+#define CARD_READER_H_
+
+/*******************************************************************************
+ * INCLUDE HEADER FILES
+ ******************************************************************************/
+
+#include "gpio.h"
+
+/*******************************************************************************
+ * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
+ ******************************************************************************/
+
+/*******************************************************************************
+ * FUNCTION PROTOTYPES WITH GLOBAL SCOPE
+ ******************************************************************************/
+
+void card_reader_INIT(pin_t enablePin, pin_t clockPin, pin_t dataPin);
+
+bool data_ready(void);
+
+uint8_t get_data_length(void);
+uint32_t * get_data(void);
+
+
+
+#endif /* CARD_READER_H_ */
